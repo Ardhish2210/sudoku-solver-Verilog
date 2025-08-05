ae85b94a133db5dcfd86421e2cd14ad3c8517072
Ardhish Patel <ardhishp@gmail.com>
2025-08-06 02:18:15 +0530

    Memory allocated

--- board_memory.v
+++ board_memory.v
@@ -5,10 +5,16 @@ input [6:0] cell_index;
 input [3:0] data_in;
 output reg [3:0] data_out;
 
+reg [3:0] board [0:80]; // 9x9 Sudoku Solver
+
 always @(posedge clk or negedge rst) begin
   if(rst) begin
     read_en <= 0;
     write_en <=0;
+    cell_index <= 7'b0000000;
+    data_in <= 4'b0000;
+  end else begin
+    
   end
 end
     
