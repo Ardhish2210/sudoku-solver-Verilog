8fc978e396a8afd3b9c7b0b8fbd869c54ad843f4
Ardhish Patel <ardhishp@gmail.com>
2025-08-06 02:14:51 +0530

    started implementing the core logic of SUDOKU board

--- board_memory.v
+++ board_memory.v
@@ -3,6 +3,13 @@ module board_memory (clk, rst, read_en, write_en, cell_index, data_in, data_out)
 input clk, rst, read_en, write_en;
 input [6:0] cell_index;
 input [3:0] data_in;
-output data_out;
+output reg [3:0] data_out;
+
+always @(posedge clk or negedge rst) begin
+  if(rst) begin
+    read_en <= 0;
+    write_en <=0;
+  end
+end
     
 endmodule
