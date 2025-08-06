ea29b68718264177c7c1ad0ed76a79e2cf8c808c
Ardhish Patel <ardhishp@gmail.com>
2025-08-07 02:00:20 +0530

    Inputs declared

--- constraint_checker.v
+++ constraint_checker.v
@@ -1,4 +1,7 @@
-module constraint_checker (
-);
-    
+module constraint_checker (num_to_place, cell_index, board, valid);
+
+input [3:0] num_to_place;
+input [6:0] cell_index;
+input [3:0] board [80:0];
+output valid;     
 endmodule
