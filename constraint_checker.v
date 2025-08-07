7149e98298dc9a2083d42fe2a7234d59c2573aea
Ardhish Patel <ardhishp@gmail.com>
2025-08-08 01:57:41 +0530

    Logic for row and column added

--- constraint_checker.v
+++ constraint_checker.v
@@ -2,6 +2,11 @@ module constraint_checker (num_to_place, cell_index, board, valid);
 
 input [3:0] num_to_place;
 input [6:0] cell_index;
-input [3:0] board [80:0];
+input [3:0] board [0:80];
 output valid;     
+
+row = cell_index/9;
+col = (cell_index % 9);
+
+
 endmodule
