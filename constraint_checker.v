ebf8e2c02f9bb8be503981542bbe5eda620e7220
Ardhish Patel <ardhishp@gmail.com>
2025-08-08 16:29:00 +0530

    Coded logic for same row and different column

--- constraint_checker.v
+++ constraint_checker.v
@@ -5,8 +5,21 @@ input [6:0] cell_index;
 input [3:0] board [0:80];
 output valid;     
 
-row = cell_index/9;
-col = (cell_index % 9);
+assign row = cell_index/9;
+assign col = (cell_index % 9);
+
+always @(*) begin
+    for (integer i = 0; i < 9; i = i + 1) begin // i = ROW
+        for(integer j = 0; j < 9; j = j + 1) begin // j = COLUMN
+            integer index = i * 9 + j;
+            if((board[index] == num_to_place) && (index != cell_index)) begin
+                valid = 0;
+            end else begin
+                valid = 1;
+            end
+        end
+    end
+end
 
 
 endmodule
