167969d2ae10b39a615bb9a7c53a528b0cd522c9
Ardhish Patel <ardhishp@gmail.com>
2025-08-08 17:06:26 +0530

    Minor errors resolved

--- constraint_checker.v
+++ constraint_checker.v
@@ -1,13 +1,14 @@
-module constraint_checker (num_to_place, cell_index, board, valid);
+module constraint_checker (num_to_place, cell_index, valid);
 
 input [3:0] num_to_place;
 input [6:0] cell_index;
-input [3:0] board [0:80];
+reg [3:0] board [0:80];
 output reg valid;     
 
 integer row, col;
 integer index;
-
+integer start_row, start_col;
+integer i, j;
 
 always @(*) begin
 
@@ -16,7 +17,7 @@ always @(*) begin
 
 //  For ROW checking
     for (integer i = 0; i < 9; i = i + 1) begin
-        index = row*9 + 1;
+        index = row*9 + i;
         if((board[index] == num_to_place) && index!= cell_index) begin
             valid = 0;
         end
@@ -34,8 +35,8 @@ always @(*) begin
     start_col = (col / 3)*3;
 
 //  For 3x3 box checking
-    for (i = start_row; i = start_row + 3; i = i + 1) begin
-        for (j = start_col; j = start_col + 3; j = j + 1) begin
+    for (i = start_row; i < start_row + 3; i = i + 1) begin
+        for (j = start_col; j < start_col + 3; j = j + 1) begin
             index = i*9 + j;
             if((board[index] == num_to_place) && index!= cell_index) begin
                 valid = 0;
