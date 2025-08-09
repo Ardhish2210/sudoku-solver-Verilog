efa828ec78a3648be9e810657398bbddc143ea96
Ardhish Patel <ardhishp@gmail.com>
2025-08-09 20:19:30 +0530

    verilog does not support "input [3:0] board [0:80]" and hence I flattened the array and then initialised it

--- board_memory.v
+++ board_memory.v
@@ -1,19 +1,19 @@
-module board_memory (clk, rst, read_en, write_en, cell_index, data_in, data_out);
+module board_memory (clk, rst, read_en, write_en, cell_index, data_in, data_out, board_flat);
 
 input clk, rst, read_en, write_en;
 input [6:0] cell_index;
 input [3:0] data_in;
 output reg [3:0] data_out;
+output [323:0] board_flat; // This is the flattened version of the 9x9 sudoku board
+integer k;
 
 reg [3:0] board [0:80]; // 9x9 Sudoku Solver
 
 always @(posedge clk or negedge rst) begin
   if(rst) begin
-    // read_en <= 0;
-    // write_en <=0;
-    // cell_index <= 7'b0000000;
-    // data_in <= 4'b0000;
-    data_out <= 4'b0000;
+    for(k = 0; k < 81; k = k +1) begin
+      board[k] <= 4'b0000;
+    end
   end else begin
     if (write_en) begin
         board[cell_index] <= data_in;
@@ -22,5 +22,12 @@ always @(posedge clk or negedge rst) begin
     end
   end
 end
-    
+  
+genvar index;
+generate 
+  for(index = 0; index < 81; index = index + 1) begin: flatten_loop
+    assign board_flat[index*4 +: 4] = board[index];
+  end
+endgenerate
+
 endmodule
